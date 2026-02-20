b051ab7b5d46bcdfc503e5a0c6905607fd71e5f1
Faizal sharif Ansari <fankstar432@gmail.com>
2026-02-21 00:38:26 +0530

    initial commit

---
+++ app/tc_test/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+export default function Test() {
+  const start = () => {
+    window.location =
+      "truecallersdk://truesdk/web_verify?type=btmsheet"
+      + "&requestNonce=12345678"
+      + "&partnerKey=p6Zcx4868bc93774f4d97977dd3642db09e60"
+      + "&partnerName=Taskify%20otpless%20login"
+      + "&lang=en"
+      + "&privacyUrl=https://crud-next-js-beta.vercel.app/privacy"
+      + "&termsUrl=https://crud-next-js-beta.vercel.app/terms"
+      + "&loginPrefix=continue"
+      + "&loginSuffix=verifymobile"
+      + "&ctaPrefix=continuewith"
+      + "&ctaColor=%2300a884"
+      + "&ctaTextColor=%23ffffff"
+      + "&btnShape=round"
+      + "&skipOption=manualdetails"
+      + "&ttl=10000";
+  };
+
+  return <button onClick={start}>Start Truecaller</button>;
+}
