892825eb8c9dcada88942e668429c3a9b7634e4d
Faizal sharif Ansari <fankstar432@gmail.com>
2026-02-21 14:51:31 +0530

    force dynamic

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -4,6 +4,8 @@ import { signIn, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import AuthDialog from "../../components/AuthDialog";
+export const dynamic = "force-dynamic";
+
 
 export default function LoginClient() {
   const { status } = useSession();
