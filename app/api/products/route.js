5ab84466be7ce1361157068feb2ee3487459014d
Faizal sharif Ansari <fankstar432@gmail.com>
2026-03-08 00:14:14 +0530

    sfsofj

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,10 +1,13 @@
-import mongoose from "mongoose";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { connectionStr } from "../../lib/mongodb";
-import { Taskify } from "../../lib/model/Product";
-import { authOptions } from "../auth/[...nextauth]/route";
+import mongoose from "mongoose";
+
 import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { cookies } from "next/headers";
+
+import { connectionStr } from "../../../lib/mongodb";
+import User from "../../../lib/model/User";
+import Taskify from "../../../lib/model/Product";
 
 export async function GET() {
   try {
